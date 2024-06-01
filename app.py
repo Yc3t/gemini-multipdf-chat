ede72c14986fdde7cc9118bc32dae20bce8da21a
Yc3t <ycetaibi@gmail.com>
2024-06-01 22:44:12 +0200

    added vector store

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 def get_pdf_text(pdf_docs):
     raw_text = ""
@@ -10,6 +14,20 @@ def get_pdf_text(pdf_docs):
             raw_text += page.extract_text()
     return raw_text 
 
+
+def get_text_chunks(text):
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=10000, chunk_overlap=1000)
+    chunks = splitter.split_text(text)
+    return chunks  # list of strings
+
+# get embeddings for each chunk
+
+def get_vector_store(chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="models/embedding-001")  # type: ignore
+    vector_store = FAISS.from_texts(chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
 
 def main():
     load_dotenv()
@@ -23,6 +41,12 @@ def main():
             with st.spinner("Processing PDFs..."):
                 raw_text  = get_pdf_text(pdf_docs)
 
+                #get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+                
+
+                #create vector store
+                vector_store = get_vector_store(text_chunks)
 
 if __name__ == '__main__':
     main()
